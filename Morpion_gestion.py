a557b084e1608b0823ba915deb36d116e975291a
Allo Scott <scott.allo@laplateforme.io>
2024-11-19 10:08:43 +0100

    Gestion de la partie

---
+++ Morpion_gestion.py
@@ -0,0 +1,35 @@
+"""Gestion de la partie"""
+
+ #Par défault, personne ne gagne.
+
+tour = 1 #Note : il y a 9 tours
+
+
+""" POUR SAVOIR SI QUELQU'UN A GAGNER :
+
+if grid[0] == joueur_2 and grid[3] == joueur_2 and grid[6] == joueur_2:
+            print("\n""joueur 2 a gagné")
+            break
+    
+        if grid[1] == joueur_2 and grid[4] == joueur_2 and grid[7] == joueur_2:
+            print("\n""joueur 2 a gagné")
+            break
+    
+        if grid[2] == joueur_2 and grid[5] == joueur_2 and grid[8] == joueur_2:
+            print("\n""joueur 2 a gagné")
+            break
+
+        #VICTOIRES LIGNES HORIZONTALES
+
+        if grid[0] == joueur_2 and grid[1] == joueur_2 and grid[2] == joueur_2:
+            print("\n""joueur 2 a gagné")
+            break
+    
+        if grid[3] == joueur_2 and grid[4] == joueur_2 and grid[5] == joueur_2:
+            print("\n""joueur 2 a gagné")
+            break
+
+        if grid[6] == joueur_2 and grid[7] == joueur_2 and grid[8] == joueur_2:
+            print("\n""joueur 2 a gagné")
+            break
+"""
